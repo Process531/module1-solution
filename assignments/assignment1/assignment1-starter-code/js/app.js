e0289c93285c4bf698727507d919d8e31d30ac4b
Process531 <Process531@users.noreply.github.com>
2017-03-06 01:47:17 +0200

    Implemented checkAmount

--- assignments/assignment1/assignment1-starter-code/js/app.js
+++ assignments/assignment1/assignment1-starter-code/js/app.js
@@ -4,11 +4,36 @@
 (function () {
     'use strict';
 
-
     angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-        .controller('LunchCheckController', function ($scope) {
+    LunchCheckController.$inject = ['$scope'];
 
-        });
+    function LunchCheckController($scope) {
+        $scope.lunchItems = "";
+        $scope.message = "";
+
+        $scope.checkAmount = function () {
+            var messageJoy = "Enjoy!";
+            var messageMuch = "Too much!";
+            var messageEnterData = "Please enter data first";
+            $scope.message = messageEnterData;
+
+            var inputArray =  $scope.lunchItems.split(', ');
+            
+            if (inputArray.length >= 1 &&  inputArray.length <= 3 && $scope.lunchItems) {
+                $scope.message = messageJoy;
+            } else  {
+                if (inputArray.length > 3)
+                    $scope.message = messageMuch;
+            }
+
+
+            return $scope.message;
+
+        }
+
+
+    }
 
 })();
