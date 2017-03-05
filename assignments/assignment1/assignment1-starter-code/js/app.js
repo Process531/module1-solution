bb85b3a8f837d916fc2b7100b2659cbe9e39354c
Process531 <Process531@users.noreply.github.com>
2017-03-06 00:54:19 +0200

    added base template. AngularJS. LunchCheck module

---
+++ assignments/assignment1/assignment1-starter-code/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Process531 on 06-Mar-17.
+ */
+(function () {
+    'use strict';
+
+
+    angular.module('LunchCheck', [])
+
+        .controller('LunchCheckController', function ($scope) {
+
+        });
+
+})();
